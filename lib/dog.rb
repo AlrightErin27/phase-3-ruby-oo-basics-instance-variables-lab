af4018952af669ce93313a22567b258e9c1a4de8
Erin Van Brunt <erinmontybruce@gmail.com>
2022-04-11 15:05:08 -0700

    done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+  # setter method
+  def name=(value)
+    @name = value
+  end
 
+  # getter method
+  def name
+    # access the data from the @name instance variable and return it
+    @name
+  end
 end
+
+illy = Dog.new
+illy.name = 'Illy'
+puts illy.name
